216769d07c319e3711e99541209275c0cec68202
רועי גורטלר <roy.gortler@post.runi.ac.il>
2024-11-24 16:00:54 +0200

    test

--- LoanCalc.java
+++ LoanCalc.java
@@ -28,8 +28,13 @@ public class LoanCalc {
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
-		// Replace the following statement with your code
-		return 0;
+		double loan1=loan,rate1=rate,payment1=payment;
+		int x=n;
+		for (int i=1;i<=n;i++)
+		loan1=(loan1-payment1)*rate1;
+		return loan1;
+		
+
 	}
 	
 	// Uses sequential search to compute an approximation of the periodical payment
@@ -38,8 +43,13 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		// Replace the following statement with your code
-		return 0;
+		double c=1000.0;
+		double sum=(endBalance(loan, rate, n, c));
+		double epsilon1=epsilon;
+		while(sum>0){iterationCounter++;
+			c+=epsilon1;
+			sum=(endBalance(loan, rate, n,c));
+		}return c;
     }
     
     // Uses bisection search to compute an approximation of the periodical payment 
@@ -48,7 +58,24 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
-		return 0;
+       double h=loan*rate*n;
+	   double l=0;
+	   double g=h+l/2;
+	   iterationCounter=0;
+	   double sum=(endBalance(loan, rate, n, g));
+	   while(h-l>epsilon){iterationCounter++;
+		sum=(endBalance(loan, rate, n, g));
+		
+		if(sum>0){
+			l=g;
+			g=(l+h)/2;
+		}else 
+			{h=g;
+				g=(h+l)/2;
+
+			}sum=(endBalance(loan, rate, n, g));
+	   }
+	  
+	   return g;
     }
 }
